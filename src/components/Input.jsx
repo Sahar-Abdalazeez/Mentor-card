1d8a2d6ee4648ea5415d785dc6c005c8d42568dd
sahar <saharabdalazeez97@gmail.com>
2022-11-08 17:25:51 +0200

    input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin-bottom:30px;
+  min-block-size: fit-content;
+  border-width: 1px;
+`;
+
+const Label = styled.div`
+  text-align: left;
+  align-self: baseline;
+  margin-bottom: 10px;
+`;
+
+const InputField = styled.input`
+  background-color: white;
+  border: solid 1px #bfbfbf;
+  height: 40px;
+  border-radius: 10px;
+  padding:10px 12px;
+  width:100%;
+  font-size: 18px;
+
+`;
+
+export const Input = (props) => {
+    return (
+        <Container style={props.style}>
+            <Label>{props.label}</Label>
+            <InputField placeholder={props.placeholder} />
+        </Container>
+    )
+}

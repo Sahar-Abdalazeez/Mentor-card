2be147472ec97b751a2ff8cef0bb37a74c67f1f0
sahar <saharabdalazeez97@gmail.com>
2022-11-09 23:33:36 +0200

    Home screen fixes

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,25 +2,22 @@ import styled from 'styled-components';
 import { Left } from '../components/Left';
 import { Right } from '../components/Right';
 import { ColoredCard } from '../components/ColoredCard';
-
+import { BlackCard } from '../components/BlackCard';
 const Container = styled.div`
   display: flex;
   width:  100%;
   height: 929px;
   flex-direction: row;
+  position: relative;
 `;
 
-const CreditCard = styled(ColoredCard)`
-/* position: absolute;
-z-index: 2; */
-width: 400px;
-`;
 export const Home = () => {
     return (
         <Container >
             <Left />
             <Right />
-            <CreditCard />
+            <ColoredCard style={{ position: "absolute", zIndex: 2, top: 200, left: 200 }} />
+            <BlackCard style={{ position: "absolute", zIndex: 2, top: 500, left: 280 }} />
         </Container>
     )
 }

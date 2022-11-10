8c49054c84d96260fcfdaad61e45ab5112d612b7
sahar <saharabdalazeez97@gmail.com>
2022-11-11 00:24:58 +0200

    adding on change functions to all inputs

--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -34,21 +34,34 @@ const DateLabelContainer = styled.div`
 `;
 const Label = styled.div``;
 
-export const Right = () => {
+export const Right = (props) => {
     return (
         <RightContainer>
             <DetailsContainer>
-                <Input label="CARDHOLDER NAME:" placeholder="ex: Jane Appleseed" />
-                <Input label="CARD NUMBER:" placeholder="1234 5678 9123 0000" />
+                <Input label="CARDHOLDER NAME:" placeholder="ex: Jane Appleseed"
+                    onChange={(name) => {
+                        props.onNameChange(name)
+                    }} />
+                <Input label="CARD NUMBER:" placeholder="1234 5678 9123 0000"
+                    onChange={(num) => {
+                        props.onNumChange(num)
+                    }} />
                 <InputsContainer>
                     <DateLabelContainer>
                         <Label>EXP,DATE (MM/YY)</Label>
                         <DateContainer>
-                            <Input style={{ width: 100, marginRight: 20 }} placeholder="MM" />
-                            <Input style={{ width: 100 }} placeholder="YY" />
+                            <Input style={{ width: 100, marginRight: 20 }} placeholder="MM" onChange={(month) => {
+                                props.onMonthChange(month)
+                            }} />
+                            <Input style={{ width: 100 }} placeholder="YY" onChange={(year) => {
+                                props.onCVCChange(year)
+                            }}
+                            />
                         </DateContainer>
                     </DateLabelContainer>
-                    <Input style={{ width: 500 }} label="CVC" placeholder="e.g 123" />
+                    <Input style={{ width: 500 }} label="CVC" placeholder="e.g 123" onChange={(cvc) => {
+                        props.onCVCChange(cvc)
+                    }} />
                 </InputsContainer>
             </ DetailsContainer >
         </RightContainer >
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
-import { Left } from '../components/Left';
-import { Right } from '../components/Right';
-import { ColoredCard } from '../components/ColoredCard';
-import { BlackCard } from '../components/BlackCard';
+import styled from "styled-components";
+import { Left } from "../components/Left";
+import { Right } from "../components/Right";
+import { ColoredCard } from "../components/ColoredCard";
+import { BlackCard } from "../components/BlackCard";
 const Container = styled.div`
   display: flex;
-  width:  100%;
+  width: 100%;
   height: 929px;
   flex-direction: row;
   position: relative;
@@ -13,11 +13,26 @@ const Container = styled.div`
 
 export const Home = () => {
     return (
-        <Container >
+        <Container>
             <Left />
-            <Right />
-            <ColoredCard style={{ position: "absolute", zIndex: 2, top: 200, left: 200 }} />
-            <BlackCard style={{ position: "absolute", zIndex: 2, top: 500, left: 280 }} />
+            <Right
+                onNumChange={(num) => console.log("this is testing ", num.target.value)}
+                onNameChange={(name) =>
+                    console.log("this is testing ", name.target.value)
+                }
+                onMonthChange={(month) => console.log(month.target.value)}
+                onYearChange={(year) => console.log(year.target.value)}
+                onCVCChange={(cvc) => console.log(cvc.target.value)}
+
+            />
+            <ColoredCard
+
+                cardNum={4111111111111}
+                style={{ position: "absolute", zIndex: 2, top: 200, left: 200 }}
+            />
+            <BlackCard
+                style={{ position: "absolute", zIndex: 2, top: 500, left: 280 }}
+            />
         </Container>
-    )
-}
+    );
+};

f9ceee07b6aa5055e9d62290c1c413ce12dc9917
sahar <saharabdalazeez97@gmail.com>
2022-11-12 21:39:53 +0200

    fix inputs and adding errors

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import "../fonts/SpaceGrotesk-Bold.ttf"
 import "../fonts/SpaceGrotesk-SemiBold.ttf"
 
 const Container = styled.div`
-  margin-bottom:30px;
+  margin-bottom:20px;
   min-block-size: fit-content;
   border-width: 1px;
 `;
@@ -13,25 +14,30 @@ const Label = styled.div`
   align-self: baseline;
   margin-bottom: 10px;
   font-family: "Medium";
-  font-size: 18px;
+  font-size: 14px;
+  letter-spacing: 2px ;
 `;
 
 const InputField = styled.input`
   background-color: white;
-  border: solid 1px #bfbfbf;
+  border-width: 1px;
   height: 40px;
   border-radius: 10px;
   padding:10px 12px;
   width:100%;
   font-size: 18px;
-
 `;
-
+const Error = styled.text`
+color: red;
+font-size:14px ;
+`;
 export const Input = (props) => {
+  const [isFocused, setIsFocused] = useState(false)
   return (
     <Container style={props.style}>
       <Label>{props.label}</Label>
-      <InputField placeholder={props.placeholder} maxLength={props.type === 'card' ? 16 : ''} onChange={(text) => props.onChange(text)} value={props.value} />
+      <InputField onBlur={() => setIsFocused(false)} onFocus={() => setIsFocused(true)} style={props.error ? { borderColor: 'red' } : isFocused ? { borderColor: '   hsl(278, 94%, 30%)' } : { borderColor: ' hsl(270, 3%, 87%)' }} placeholder={props.placeholder} maxLength={props.type === 'card' ? 16 : ''} onChange={(text) => props.onChange(text)} value={props.value} />
+      <Error>{props.error}</Error>
     </Container>
   )
 }
--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -1,76 +1,178 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import { Input } from '../components/Input';
+import { useState } from "react";
+import styled from "styled-components";
+import { Input } from "../components/Input";
 
 const RightContainer = styled.div`
-width: 100%;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-min-width: 600px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-width: 600px;
 `;
 
 const InputsContainer = styled.div`
- display: flex;
- justify-content:space-between ;
-  flex:1;
+  display: flex;
+  justify-content: space-between;
+  flex: 1;
   width: 100%;
 `;
 
 const DateContainer = styled.div`
- display: flex;
-justify-content: space-between;
-align-items: center;
-width:80%;
-margin-right: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+  margin-right: 50px;
 `;
 
 const DetailsContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 30%;
-  min-width:400px ;
+  min-width: 400px;
 `;
-const DateLabelContainer = styled.div`
-  
-`;
+const DateLabelContainer = styled.div``;
 
 const Label = styled.div`
-/* font-family:'SpaceGrotesk-' ; */
+  font-family: "Medium";
+  font-size: 14px;
+  letter-spacing: 2px;
 `;
+const Confirm = styled.button`
+  width: "100%";
+  height: 50px;
+  background-color: hsl(278, 68%, 11%);
+  border-radius: 10px;
+  border-color: hsl(278, 68%, 11%);
+`;
+// const ConfirmText =
 
 export const Right = (props) => {
-    const [cardNum, setCardNum] = useState();
+    const [fieldsValues, setFieldsValues] = useState({
+        name: "",
+        number: "",
+        month: "",
+        year: "",
+        cvc: "",
+    });
+    // const [focus, setFocus] = useState({ name: false, number: false, month: false, year: false, cvc: false });
+    const [error, setError] = useState({
+        name: false,
+        number: false,
+        month: false,
+        year: false,
+        cvc: false,
+    });
+
+    const getError = () => {
+        if (!fieldsValues.name.length) {
+            setError((prevError) => ({ ...prevError, name: "can't be empty" }));
+        }
+        if (!fieldsValues.number.length) {
+            setError((prevError) => ({ ...prevError, number: "can't be empty" }));
+        }
+        if (!fieldsValues.month.length) {
+            setError((prevError) => ({ ...prevError, month: "can't be empty" }));
+        }
+        if (!fieldsValues.year.length) {
+            setError((prevError) => ({ ...prevError, year: "can't be empty" }));
+        }
+        if (!fieldsValues.cvc.length) {
+            setError((prevError) => ({ ...prevError, cvc: "can't be empty" }));
+        }
+    };
     return (
         <RightContainer>
             <DetailsContainer>
-                <Input label="CARDHOLDER NAME:" placeholder="ex: Jane Appleseed"
+                <Input
+                    error={error.name}
+                    label="CARDHOLDER NAME:"
+                    placeholder="ex: Jane Appleseed"
                     onChange={(name) => {
-                        props.onNameChange(name)
-                    }} />
-                <Input type="card" label="CARD NUMBER:" placeholder="1234 5678 9123 0000"
+                        props.onNameChange(name);
+                        setError((prevError) => ({ ...prevError, name: false }));
+
+                        setFieldsValues((prevActivatedItem) => ({
+                            ...prevActivatedItem,
+                            name: name.target.value,
+                        }));
+                    }}
+                />
+                <Input
+                    error={error.number}
+                    isFocused={true}
+                    type="card"
+                    label="CARD NUMBER:"
+                    placeholder="1234 5678 9123 0000"
                     onChange={(num) => {
                         props.onNumChange(num);
-                    }} />
+                        setError((prevError) => ({ ...prevError, number: false }));
+
+                        setFieldsValues((prevActivatedItem) => ({
+                            ...prevActivatedItem,
+                            number: num.target.value,
+                        }));
+                    }}
+                />
                 <InputsContainer>
                     <DateLabelContainer>
                         <Label>EXP.DATE (MM/YY)</Label>
                         <DateContainer>
-                            <Input style={{ width: 100, marginRight: 20 }} placeholder="MM" onChange={(month) => {
-                                props.onMonthChange(month)
-                            }} />
-                            <Input style={{ width: 100 }} placeholder="YY" onChange={(year) => {
-                                props.onYearChange(year)
-                            }}
+                            <Input
+                                error={error.month}
+                                style={{ width: 100, marginRight: 20 }}
+                                placeholder="MM"
+                                onChange={(month) => {
+                                    props.onMonthChange(month);
+                                    setError((prevError) => ({ ...prevError, month: false }));
+
+                                    setFieldsValues((prevActivatedItem) => ({
+                                        ...prevActivatedItem,
+                                        month: month.target.value,
+                                    }));
+                                }}
+                            />
+                            <Input
+                                error={error.year}
+                                style={{ width: 100 }}
+                                placeholder="YY"
+                                onChange={(year) => {
+                                    props.onYearChange(year);
+                                    setError((prevError) => ({ ...prevError, year: false }));
+
+                                    setFieldsValues((prevActivatedItem) => ({
+                                        ...prevActivatedItem,
+                                        year: year.target.value,
+                                    }));
+                                }}
                             />
                         </DateContainer>
                     </DateLabelContainer>
-                    <Input style={{ width: 500 }} label="CVC" placeholder="e.g 123" onChange={(cvc) => {
-                        props.onCVCChange(cvc)
-                    }} />
+                    <Input
+                        error={error.cvc}
+                        style={{ width: 500 }}
+                        label="CVC"
+                        placeholder="e.g 123"
+                        onChange={(cvc) => {
+                            props.onCVCChange(cvc);
+                            setError((prevError) => ({ ...prevError, cvc: false }));
+
+                            setFieldsValues((prevActivatedItem) => ({
+                                ...prevActivatedItem,
+                                cvc: cvc.target.value,
+                            }));
+                        }}
+                    />
                 </InputsContainer>
-            </ DetailsContainer >
-        </RightContainer >
+                <Confirm
+                    title="Confirm"
+                    style={{ color: "white", fontSize: 18 }}
+                    onClick={() => getError()}
+                >
+                    Confirm
+                </Confirm>
+            </DetailsContainer>
+        </RightContainer>
     );
-}
+};

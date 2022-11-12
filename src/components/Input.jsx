45fd134840b17c1d93fcba267db95bc0ac6cd7ad
sahar <saharabdalazeez97@gmail.com>
2022-11-12 13:11:30 +0200

    adding maxlength

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import "../fonts/SpaceGrotesk-Bold.ttf"
+import "../fonts/SpaceGrotesk-SemiBold.ttf"
 
 const Container = styled.div`
   margin-bottom:30px;
@@ -10,6 +12,8 @@ const Label = styled.div`
   text-align: left;
   align-self: baseline;
   margin-bottom: 10px;
+  font-family: "Medium";
+  font-size: 18px;
 `;
 
 const InputField = styled.input`
@@ -27,7 +31,7 @@ export const Input = (props) => {
   return (
     <Container style={props.style}>
       <Label>{props.label}</Label>
-      <InputField placeholder={props.placeholder} onChange={(text) => props.onChange(text)} />
+      <InputField placeholder={props.placeholder} maxLength={props.type === 'card' ? 16 : ''} onChange={(text) => props.onChange(text)} value={props.value} />
     </Container>
   )
 }

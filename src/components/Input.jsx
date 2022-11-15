ef48368a8d20211ed44fae296f91040a5fd30c9a
sahar <saharabdalazeez97@gmail.com>
2022-11-16 00:10:03 +0200

    prettier format input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import "../fonts/SpaceGrotesk-Bold.ttf"
-import "../fonts/SpaceGrotesk-SemiBold.ttf"
+import { useState } from "react";
+import styled from "styled-components";
+//fonts
+import "../fonts/SpaceGrotesk-Bold.ttf";
+import "../fonts/SpaceGrotesk-SemiBold.ttf";
+//util
+import { useWindowSize } from "../utils/screen.utils";
 
 const Container = styled.div`
-  margin-bottom:20px;
+  margin-bottom: 20px;
   min-block-size: fit-content;
-  border-width: 1px;
+  margin-bottom: ${(props) => (props.error ? 0 : 20)}px;
 `;
 
 const Label = styled.div`
@@ -15,29 +18,60 @@ const Label = styled.div`
   margin-bottom: 10px;
   font-family: "Medium";
   font-size: 14px;
-  letter-spacing: 2px ;
+  letter-spacing: 2px;
+  height: 20px;
 `;
 
 const InputField = styled.input`
-  background-color: white;
-  border-width: 1px;
-  height: 40px;
+  border-width: 0px;
+  height: 38px;
   border-radius: 10px;
-  padding:10px 12px;
-  width:100%;
+  padding: 10px 12px;
+  width: 100%;
   font-size: 18px;
 `;
+
 const Error = styled.text`
-color: red;
-font-size:14px ;
+  color: red;
+  font-size: 14px;
+  height: 20px;
+  width: 100%;
 `;
+
+const InputContainer = styled.div`
+  height: 40px;
+  display: flex;
+  border: solid 1px black;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+`;
+
 export const Input = (props) => {
-  const [isFocused, setIsFocused] = useState(false)
+  const isMobile = useWindowSize();
+  const [isFocused, setIsFocused] = useState(false);
   return (
-    <Container style={props.style}>
+    <Container error={props.error} isMobile={isMobile} style={props.style}>
       <Label>{props.label}</Label>
-      <InputField onBlur={() => setIsFocused(false)} onFocus={() => setIsFocused(true)} style={props.error ? { borderColor: 'red' } : isFocused ? { borderColor: '   hsl(278, 94%, 30%)' } : { borderColor: ' hsl(270, 3%, 87%)' }} placeholder={props.placeholder} maxLength={props.type === 'card' ? 16 : ''} onChange={(text) => props.onChange(text)} value={props.value} />
-      <Error>{props.error}</Error>
+      <InputContainer
+        style={
+          props.error
+            ? { borderColor: "red" }
+            : isFocused
+              ? { borderColor: "   hsl(278, 94%, 30%)" }
+              : { borderColor: " hsl(270, 3%, 87%)" }
+        }
+      >
+        <InputField
+          onBlur={() => setIsFocused(false)}
+          onFocus={() => setIsFocused(true)}
+          placeholder={props.placeholder}
+          maxLength={props.maxLength}
+          onChange={(text) => props.onChange(text)}
+          value={props.value}
+        />
+      </InputContainer>
+      <Error>{props.error || ""}</Error>
     </Container>
-  )
-}
+  );
+};

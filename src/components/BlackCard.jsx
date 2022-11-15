4257250d0d3a30c45b87c99b9723e3d8201069ca
sahar <saharabdalazeez97@gmail.com>
2022-11-15 21:16:57 +0200

    add ismobile condition

--- src/components/BlackCard.jsx
+++ src/components/BlackCard.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import background from "../assets/images/bg-card-back.png";
-
+import { useWindowSize } from '../utils/screen.utils'
 const Card = styled.div`
 background-color:transparent ;
- height: 240px;
- width: 420px;
+ height: ${props => props.isMobile ? 180 : 240}px;
+ width: ${props => props.isMobile ? 280 : 420}px;
  padding:20px;
  border-radius: 10px;
 
 `;
-const Container = styled.div``;
+const Container = styled.div`
+ width: ${props => props.isMobile ? 220 : 420}px;
+`;
 
 
 export const BlackCard = ({ style }) => {
-    return (<Container style={style}>
-        <Card style={{
+    const isMobile = useWindowSize();
+    return (<Container isMobile={isMobile} style={style}>
+        <Card isMobile={isMobile} style={{
             backgroundImage: `url(${background})`, backgroundRepeat: 'no-repeat', backgroundSize: "cover", backgroundPositionX: 'center', backgroundPositionY: 'center'
 
         }} />
--- src/components/CardInput.jsx
+++ src/components/CardInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-
+import { useWindowSize } from '../utils/screen.utils'
 const Container = styled.div`
   
 `;
@@ -8,15 +8,16 @@ const Input = styled.input`
   background-color: transparent;
   width: 100%;
   border-width: 0px;
-  font-size: 30px;
+  font-size: ${props => props.isMobile ? 20 : 30}px;
   pointer-events:none ;
 `;
 
 
 export const CardInput = ({ style, value }) => {
+  const isMobile = useWindowSize();
   return (
     <Container>
-      <Input style={style} id="inputID" value={value} placeholder="0000000000000000" />
+      <Input isMobile={isMobile} style={style} id="inputID" value={value} placeholder="0000000000000000" />
     </Container>
   )
 };

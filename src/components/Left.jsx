6d4f06f2f8ca872a00be562357d01d27688656aa
sahar <saharabdalazeez97@gmail.com>
2022-11-16 00:19:48 +0200

    format left

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -1,20 +1,23 @@
-import styled from 'styled-components';
-import { useWindowSize } from '../utils/screen.utils';
+import styled from "styled-components";
+//util
+import { useWindowSize } from "../utils/screen.utils";
 
 const LeftContainer = styled.div`
- height:${props => props.isMobile ? "20%" : "100%"};
- /* width: ${props => props.isMobile ? 375 : ' 30%'}; */
- display: flex;
- min-width: ${props => props.isMobile ? 375 : 480}px;
+  height: ${(props) => (props.isMobile ? "20%" : "100%")};
+  display: flex;
+  min-width: ${(props) => (props.isMobile ? 375 : 480)}px;
 `;
-
 
 export const Left = (props) => {
     const isMobile = useWindowSize();
 
     return (
-
-        <LeftContainer isMobile={isMobile} style={props.style} >
-            <img className="profile-photo" src={require("../assets/images/bg-main-desktop.png")} alt={"Carlie Anglemire"} />
-        </LeftContainer>);
-}
+        <LeftContainer isMobile={isMobile} style={props.style}>
+            <img
+                className="profile-photo"
+                src={require("../assets/images/bg-main-desktop.png")}
+                alt={"Carlie Anglemire"}
+            />
+        </LeftContainer>
+    );
+};
